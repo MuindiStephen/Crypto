f03cc5663479f6ec2a1050206479f7933f4e57a8
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-01 22:34:25 +0300

    worked on UseCases for business Logic

--- app/src/main/java/com/steve_md/crypto_app/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/steve_md/crypto_app/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -1,23 +1,24 @@
-package com.steve_md.crypto_app.domain.use_case.get_coins
+package com.steve_md.crypto_app.domain.use_case.get_coin
 
 import com.steve_md.crypto_app.common.Resource
 import com.steve_md.crypto_app.data.remote.dto.toCoin
+import com.steve_md.crypto_app.data.remote.dto.toCoinDetail
 import com.steve_md.crypto_app.domain.model.Coin
+import com.steve_md.crypto_app.domain.model.CoinDetail
 import com.steve_md.crypto_app.domain.repository.CoinRepository
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
-class GetCoinsUseCase @Inject constructor(
+class GetCoinUseCase @Inject constructor(
     private val coinRepository: CoinRepository
 ) {
-     operator fun invoke() : Flow<Resource<List<Coin>>> = flow {
+     operator fun invoke(coinId : String) : Flow<Resource<CoinDetail>> = flow {
         try {
             emit(Resource.Loading())
-            val coins = coinRepository.getCoins().map { it.toCoin() }
+            val coins = coinRepository.getCoinById(coinId).toCoinDetail()
             Resource.Success(coins)
         } catch (e:HttpException) {
             emit(Resource.Error(e.localizedMessage?: "An unexpected error has occurred!"))

89385702b5a6f7c5bc860776a1c0aede5bd208af
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-06 20:55:25 +0300

    some changes

--- app/src/main/java/com/steve_md/crypto_app/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/steve_md/crypto_app/domain/repository/CoinRepository.kt
@@ -1,8 +1,11 @@
 package com.steve_md.crypto_app.domain.repository
 
-import com.steve_md.crypto_app.common.Resource
 import com.steve_md.crypto_app.data.remote.dto.CoinDetailDto
 import com.steve_md.crypto_app.data.remote.dto.CoinDto
+
+/* a version of the Implementation
+* Ease code unit testing
+*/
 
 interface CoinRepository {
     suspend fun getCoins() : List<CoinDto>

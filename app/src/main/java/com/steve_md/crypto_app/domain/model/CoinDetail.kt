f50f55a3177c0cfbb5c17bfcc350a288d15e7ba9
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-13 12:03:09 +0300

    some changes

--- app/src/main/java/com/steve_md/crypto_app/domain/model/CoinDetail.kt
+++ app/src/main/java/com/steve_md/crypto_app/domain/model/CoinDetail.kt
@@ -1,14 +1,14 @@
 package com.steve_md.crypto_app.domain.model
 
-import com.steve_md.crypto_app.data.remote.dto.Team
+import com.steve_md.crypto_app.data.remote.dto.TeamMember
 
 data class CoinDetail(
-    val coinId : String,
-    val name : String,
+    val coinId: String,
+    val name: String,
     val description: String,
     val symbol: String,
     val rank: Int,
     val isActive: Boolean,
     val tags: List<String>,
-    val team: Team
+    val team: List<TeamMember>
 )
